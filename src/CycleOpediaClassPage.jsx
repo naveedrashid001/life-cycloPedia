c5ec74beec071fa684b622c685cb36b053022492
Naveed Rashid <149875690+naveedrashid001@users.noreply.github.com>
2024-08-19 23:28:53 -0700

    i create a new folder cyclopedia coding

---
+++ src/CycleOpediaClassPage.jsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class CycleOpediaClassPage extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            instructor:undefined,
+            studentList:[],
+            studentCount:0,
+            hideInstructor:false
+         }
+
+    }
+
+    render(){
+    
+    }
+}
+
