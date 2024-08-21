0c7be392dbcb2cb015ba4e535fea60fb947452dc
Naveed Rashid <149875690+naveedrashid001@users.noreply.github.com>
2024-08-21 05:10:02 -0700

    i created a two button one for add student and another for remove all students

--- src/CycleOpediaClassPage.jsx
+++ src/CycleOpediaClassPage.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { getRandom } from "./utility/api";
 import React from "react";
 
 class CycleOpediaClassPage extends React.Component{
@@ -13,9 +13,72 @@ class CycleOpediaClassPage extends React.Component{
          }
 
     }
+    componentDidMount =async ()=>{
+        console.log("component Did Mount")
+        const response = await getRandom();
+        console.log(response)
+        this.setState((prevstate)=>{
+            return {
+                instructor:{
+                    name :response.data.first_name +" "+ response.data.first_name,
+                    email :response.data.email,
+                    phone :response.data.phone_number,
+                }
+            }
+        })
+        // return <div>this.instructor</div>
+        }
+    
+    componentDidUpdate=()=>{
+        console.log("component Did Update")
+    }
+    componentDidUnMount=()=>{
+        console.log("component Did Un Mount")
+    };
+    //  add student button 
+    HandleAddStudent =()=>{
+        this.setState((prevstate)=>{
+            return {
+                studentCount : prevstate.studentCount+1
+            }
+        })
+    }
+        //  remove all student button 
+    HandleRemoveAddStudent =()=>{
+        this.setState((prevstate)=>{
+            return {
+                studentCount :0
+            }
+        })
+    }
 
     render(){
-    
+    console.log("Render component");
+    return (<div>
+        {
+            this.state.instructor && (
+                <div className="p-3">
+                   <span className="h4 text-success">instructor</span> 
+                   <i className="bi bi-toggle-on"></i>
+                   <br />
+                   Name: {this.state.instructor.name} <br />
+                   Email: {this.state.instructor.email}
+                   <br />
+                   Phone: {this.state.instructor.phone}
+                </div>
+            ) }
+
+<div className="p-3">
+                   <span className="h4 text-success">Students</span>
+                   <br />
+                   students Count: {this.state.studentCount} <br />
+                   <button onClick={()=>this.HandleAddStudent()} className="btn btn-success btn-sm " style={{marginRight:"5px"}}> Add Student</button>
+                   
+                   <button onClick={()=>this.HandleRemoveAddStudent()} className="btn btn-danger btn-sm"> Remove All Students</button>
+                </div>
+
+    </div>)
     }
 }
 
+export default CycleOpediaClassPage;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Header from './Header';
+import CycleOpediaClassPage from './CycleOpediaClassPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Header></Header>);
+root.render(<div>
+  <Header></Header>
+  <div className='row text-white'> 
+    <div className='col-6'>
+      <span className='h1 text-warning text-center'>Class Component</span>
+      <CycleOpediaClassPage/>
+    </div>
+
+
+  </div>
+</div>
+
+);
 
 
