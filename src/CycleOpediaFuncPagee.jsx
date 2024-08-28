09ac8977b5fd0d3414eb95cf86cb584428c2e43f
Naveed Rashid <149875690+naveedrashid001@users.noreply.github.com>
2024-08-28 05:55:18 -0700

    now i add functional component and compare it with class bases component

---
+++ src/CycleOpediaFuncPagee.jsx
@@ -0,0 +1,101 @@
+import Instructor from "./instructor";
+import { getRandom } from "./utility/api";
+import React, { useState } from "react";
+
+const CycleOpediaFuncPagee = () => {
+
+    const [state, setState] = useState({
+        instructor: null, // Initialize as null
+        studentList: [],
+        studentCount: 0,
+        hideInstructor: false,
+    });
+
+    const [inputName, setInputName] = useState("");
+    const [inputFeedback, setInputFeedback] = useState("");
+
+    const handleAddStudent = () => {
+        setState(prevState => ({
+            ...prevState,
+            studentCount: prevState.studentCount + 1,
+        }));
+    };
+
+    const handleRemoveAllStudents = () => {
+        setState(prevState => ({
+            ...prevState,
+            studentCount: 0,
+            studentList: []
+        }));
+    };
+
+    const handleToggleInstructor = () => {
+        setState(prevState => ({
+            ...prevState,
+            hideInstructor: !prevState.hideInstructor,
+        }));
+    };
+
+    return (
+        <div>
+            <div className="p-3">
+                <span className="h4 text-success">Instructor</span>{" "}
+                <i
+                    className={`bi ${
+                        state.hideInstructor
+                            ? "bi-toggle-off text-danger"
+                            : "bi-toggle-on text-success"
+                    }`}
+                    onClick={handleToggleInstructor}
+                ></i>
+                <br />
+                {!state.hideInstructor ? (
+                    <Instructor instructor={state.instructor} />
+                ) : null}
+            </div>
+            <div className="p-3">
+                <span className="h4 text-success">FeedBack</span>
+                <br />
+                <input
+                    type="text"
+                    value={inputName}
+                    placeholder="Name..."
+                    onChange={(e) => setInputName(e.target.value)}
+                />{" "}
+                value: {inputName}
+                <br />
+                <textarea
+                    value={inputFeedback}
+                    placeholder="Feedback..."
+                    onChange={(e) => setInputFeedback(e.target.value)}
+                ></textarea>{" "}
+                value: {inputFeedback}
+            </div>
+            <div className="p-3">
+                <span className="h4 text-success">Students</span>
+                <br />
+                students Count: {state.studentCount} <br />
+                <button
+                    onClick={handleAddStudent}
+                    className="btn btn-success btn-sm"
+                    style={{ marginRight: "5px" }}
+                >
+                    Add Student
+                </button>
+                <button
+                    onClick={handleRemoveAllStudents}
+                    className="btn btn-danger btn-sm"
+                >
+                    Remove All Students
+                </button>
+                {state.studentList.map((student, index) => (
+                    <div className="text-white" key={index}>
+                        - {student.name}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default CycleOpediaFuncPagee;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import Header from './Header';
 import CycleOpediaClassPage from './CycleOpediaClassPage';
+import CycleOpediaFuncPagee from './CycleOpediaFuncPagee';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div>
@@ -11,6 +12,12 @@ root.render(<div>
     <div className='col-6'>
       <span className='h1 text-warning text-center'>Class Component</span>
       <CycleOpediaClassPage/>
+    </div>
+
+    {/*  functional component Render */}
+    <div className='col-6'>
+      <span className='h1 text-warning text-center'>Class Component</span>
+      <CycleOpediaFuncPagee/>
     </div>
 
 
